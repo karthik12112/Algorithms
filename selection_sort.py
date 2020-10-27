85ddee4189a1a9033b487f76bd781d6661b6d41d
karthik <35927737+karthik12112@users.noreply.github.com>
2020-10-26 23:40:46 -0700

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,17 +1,16 @@
 
 def selection_sort(list_a):
-    indexing_length = range(0, len(list_a)-1)
-
+    indexing_length = range(0, len(list_a)-1) # [0,1,2]
     for i in indexing_length:
-        min_value = i
-
-        for j in range(i+1, len(list_a)):
-            if list_a[j] < list_a[min_value]:
+        min_value = i # 0 # 1
+        for j in range(i+1, len(list_a)): 
+            if list_a[j] < list_a[min_value]: # 7 < 6 # 0 < 7
                 min_value = j
-
-            if min_value != i:
+            if min_value != i: 
                 list_a[min_value], list_a[i] = list_a[i], list_a[min_value]
-
+        print(list_a)
     return list_a
 
-print(selection_sort([6,7,8,7,6,5,4,5,6,7,6,7,8,9,7,9,0]))
+
+
+print(selection_sort([6,7,0]))
